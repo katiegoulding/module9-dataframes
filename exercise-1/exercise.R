6978a5d04c2e0d7d75a2e6d1a615921183cee167
Katie Goulding <katiegoulding@gmail.com>
2017-01-19 15:25:13 -0800

    add mod 9

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,26 +3,47 @@
 # Create a vector of the number of points the Seahawks scored in each game this
 # season (google "Seahawks" for the info!)
 
+seahawks.points.scored <- c(20, 26, 25, 31, 24, 10, 40, 5, 26, 31, 31, 20)
+
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each game this season
 
+seahawks.points.against <- c(36, 6, 23, 34, 3, 38, 7, 14, 15, 24, 35, 25)
 
 # Combine your two vectors into a dataframe
 
+seahawks.data <- data.frame(seahawks.points.scored, seahawks.points.against)
+seahawks.data
 
 # Create a new column "diff" that is the difference in points.
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
 
+differ <- seahawks.points.scored - seahawks.points.against
+seahawks.data$diff <- differ
+print(seahawks.data)
+
+###Row and column extraction:
+seahawks.data[7, 1]
+seahawks.data[3, ]
+seahawks.data[, 2]
+seahawks.points.against
+seahawks.data[seahawks.points.scored > 20,]
+seahawks.data[1] #With holding the comma produces it like half a table defaultin to COLUMNS
 
 # Create a new column "won" which is TRUE if the Seahawks won
 
+winning <- seahawks.points.scored > seahawks.points.against
+seahawks.data$won <- winning
+seahawks.data
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponents <- c("Falcons", "Lions", "49ers", "Cardinals", "Rams", "Packers", "Panthers", "Buccaneers", "Eagles", "Patriots","Bills", "Saints")
+seahawks.data$opponents <- opponents
+View(seahawks.data)
 
 # Assign your dataframe rownames of their opponents
-
+rownames(seahawks.data) <- opponents
 
 # View your data frame to see how it has changed!
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,20 +1,27 @@
 # Exercise 6: Husky Football 2015 Season
 # Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-
+setwd("C:/Users/Katie/Documents/INFO_201/Modules/module9-dataframes/exercise-6/data")
+data <- read.csv(file = "huskies_2015.csv")
+data
 
 # Create a vector of the teams that the Huskies played against during that season
-
+opponents <- data$opponent
+opponents
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-
+husky.scores <- data$uw_score
+husky.scores
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-
+rushing.yards <- data$rushing_yards
+passing.yards <- data$passing_yards
+rushing.yards 
+passing.yards 
 
 # Create a variable called `combined.yards` that is the total yardage of the Huskies for each game
-
-
+combined.yards <- rushing.yards + passing.yards
+combined.yards
 # What is the score of the game where the Huskies had the most combined yards?
 
 
